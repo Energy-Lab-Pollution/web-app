91e11b75bfe6ab6849e0a50f9a53dfc64ae12a84
Federico Domínguez Molina <fd.molina@outlook.com>
2024-09-09 15:54:16 -0500

    Adding streamlit script

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+"""
+This script contains the necessary code for the streamlit app
+for the Energy Lab at UChicago
+"""
+
+# Global imports
+import streamlit as st
+
+# Local imports
+from util.constants import CITIES
