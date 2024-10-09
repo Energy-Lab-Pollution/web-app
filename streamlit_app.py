6ad808bde3412e94b2459bf74f10d7bdb78223cb
Federico Domínguez Molina <fd.molina@outlook.com>
2024-10-09 16:00:35 -0500

    Changing for selectbox, debug df name

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ from visualizations.create_visualizations import (create_lineplot,
 # App layout
 st.set_page_config(page_title="Pollution Monitoring App", page_icon=":bar_chart:")
 
+# Date
 today = datetime.now(pytz.timezone("America/Chicago"))
 str_today = datetime.strftime(today, "%Y-%m-%d")
 # Custom CSS for basic styling
@@ -26,20 +27,17 @@ st.title("🌍 UChicago Energy & Environment Lab - Pollution Monitoring App")
 st.sidebar.image(LOGO_URL)
 
 # Sidebar: Static vs Interactive plot selector
-plot_type = st.sidebar.radio("Choose Plot Type:", ("Static", "Interactive"))
-
-# Sidebar: Option to view data only
+plot_type = st.sidebar.selectbox("Choose Plot Type:", ("Static", "Interactive"))
 view_data_only = st.sidebar.checkbox("View Data Only")
-
-# Add a selector for the user to choose a city
 city_choice = st.selectbox("Select a city to fetch data and visualize", CITIES)
+
 
 if st.button("Run"):
     data_wrapper = DataWrapper(city_choice)
 
     if view_data_only:
         st.subheader(f"Data for {city_choice}")
-        st.dataframe(data_wrapper.air_quality_df)  # Show the dataframe
+        st.dataframe(data_wrapper.air_quality_csv)  # Show the dataframe
         st.download_button(
             label="Download Data as CSV",
             data=data_wrapper.air_quality_csv,
